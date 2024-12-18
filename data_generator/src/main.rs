4d28a569a92ed9496b4c9c93ce2a141f533868d2
nightingale38 <romain.ageron@student-cs.fr>
2024-12-18 11:52:09 +0100

    clap

--- data_generator/src/main.rs
+++ data_generator/src/main.rs
@@ -1,3 +1,31 @@
+use clap::{Parser, ValueEnum};
+use std::path::PathBuf;
+
+// TODO: validate args
+#[derive(Parser)]
+#[command(version, about)]
+struct Cli {
+    #[arg(short, long)]
+    colors: u8,
+
+    #[arg(short, long)]
+    output_file: Option<PathBuf>,
+
+    #[arg(short, long)]
+    workers: Option<usize>,
+
+    #[arg(short, long, value_enum)]
+    problem: Problem,
+}
+
+// TODO: maybe subcommands?
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, ValueEnum)]
+enum Problem {
+    Schur,
+}
+
 fn main() {
+    let cli = Cli::parse();
+
     println!("Hello, world!");
 }
