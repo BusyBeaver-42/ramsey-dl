488532a0e32a9832dbc562aa9debad5d49e622ba
nightingale38 <romain.ageron@student-cs.fr>
2024-12-19 09:06:15 +0100

    upper_bound_impl! macro

--- crates/ramsey_theory/src/problems.rs
+++ crates/ramsey_theory/src/problems.rs
@@ -13,6 +13,15 @@ pub trait UpperBound {
     const BOUND: usize;
 }
 
+#[macro_export]
+macro_rules! upper_bound_impl {
+    ($problem:ty = $bound:literal) => {
+        impl $crate::problems::UpperBound for $problem {
+            const BOUND: usize = $bound + 1;
+        }
+    };
+}
+
 pub trait SequenceProblem<const N_COLORS: usize>: UpperBound {
     fn play(
         size: &mut usize,
@@ -27,9 +36,11 @@ pub struct Schur<const N_COLORS: usize>
 where
     Self: UpperBound;
 
-impl UpperBound for Schur<1> {
-    const BOUND: usize = 2;
-}
+upper_bound_impl! { Schur<1> = 1 }
+upper_bound_impl! { Schur<2> = 4 }
+upper_bound_impl! { Schur<3> = 13 }
+upper_bound_impl! { Schur<4> = 44 }
+upper_bound_impl! { Schur<5> = 160 }
 
 impl<const N_COLORS: usize> SequenceProblem<N_COLORS> for Schur<N_COLORS>
 where
