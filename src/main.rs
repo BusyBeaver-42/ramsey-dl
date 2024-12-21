b5a42740125fe3b703fcde67866287d4fb7f5df2
nightingale38 <romain.ageron@student-cs.fr>
2024-12-21 12:13:27 +0100

    single file :D

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,38 @@
 #![allow(incomplete_features)]
 #![feature(generic_const_exprs)]
 
-use reproduce_crash::{Assert, IsTrue, Schur, SequenceProblem};
+enum Assert<const CHECK: bool> {}
+
+trait IsTrue {}
+
+impl IsTrue for Assert<true> {}
+
+trait UpperBound {
+    const BOUND: usize;
+}
+
+trait SequenceProblem: UpperBound {
+    const N_COLORS: usize;
+
+    fn play();
+}
+
+struct Schur<const N_COLORS: usize>
+where
+    Self: UpperBound;
+
+impl UpperBound for Schur<1> {
+    const BOUND: usize = 1;
+}
+
+impl<const N_COLORS: usize> SequenceProblem for Schur<N_COLORS>
+where
+    Self: UpperBound,
+{
+    const N_COLORS: usize = N_COLORS;
+
+    fn play() {}
+}
 
 fn run_<P>()
 where
@@ -12,7 +43,7 @@ where
 {
 }
 
-pub trait Run {
+trait Run {
     fn run(&self);
 }
 
